7a3b356ceb6e07e7658a6a471e495ed5e3db6cad
azhukov2204 <andrew284@mail.ri>
2021-06-07 16:20:32 +0500

    homework1

---
+++ src/homework1/Person.java
@@ -0,0 +1,31 @@
+package homework1;
+
+import java.util.Objects;
+
+public class Person {
+    private final String name;
+    private final int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Имя: " + name + " Возраст: " + age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Person)) return false;
+        Person person = (Person) o;
+        return age == person.age && Objects.equals(name, person.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age);
+    }
+}
